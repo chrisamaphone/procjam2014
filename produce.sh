86bb984ce834aa4e3352248d22aa1394723f1a39
Rob Simmons <robsimmons@gmail.com>
2014-11-09 19:52:20 +0000

    Make a local produce

--- produce.sh
+++ produce.sh
@@ -1,16 +1,20 @@
 # /bin/bash
 
 celf=/home/rjsimmon/celf/celf
+seed=`date +%Y%j%H`
 
 echo "=== Procjam production manager ==="
-echo === $(date) ===
+echo "=== $(date), producing seed ${seed} ==="
 
 for dir in /home/www/play/u/*/
 do
     dir=${dir%*/}
     name=${dir##*/}
     echo "Producing ${name} from ${dir##/home/www/}"
-    ${celf} ${dir}/${name}.clf > ${dir}/${name}.out
+    ${celf} -s ${seed} ${dir}/${name}.clf > ${dir}/${name}.out
     wc=`wc -l ${dir}/${name}.out | awk {'print $1'}`
     echo "   Produced ${name} script (${name}.clf), ${wc} lines"
 done
+
+echo "=== Procjam production manager finished ==="
+echo ""
