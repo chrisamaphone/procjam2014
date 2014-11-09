86d9453e6673586e377f9efe138cda81ff619688
Rob Simmons <robsimmons@gmail.com>
2014-11-09 15:57:35 -0500

    Production

--- produce.sh
+++ produce.sh
@@ -1,17 +1,16 @@
 # /bin/bash
 
-celf=/home/rjsimmon/celf/celf
 seed=`date +%Y%j%H`
 
 echo "=== Procjam production manager ==="
-echo "=== $(date), producing seed ${seed} ==="
+echo "=== $(date), seed ${seed} ==="
 
 for dir in world/*/
 do
     dir=${dir%*/}
     name=${dir##*/}
     echo "Producing ${name} from ${dir##/home/www/}"
-    ${celf} -s ${seed} ${dir}/${name}.clf > ${dir}/${name}.out
+    celf -s ${seed} ${dir}/${name}.clf > ${dir}/${name}.out
     wc=`wc -l ${dir}/${name}.out | awk {'print $1'}`
     echo "   Produced ${name} script (${name}.clf), ${wc} lines"
 done
